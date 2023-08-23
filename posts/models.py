b51566dfc0035c5075da6e6f321c853b3db787e7
KangMinju <demo1208@gmail.com>
2023-08-23 11:31:15 +0900

    Read(all) 기능 구현

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.Charfield()
+    title = models.CharField(max_length=50)
     content = models.TextField()
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
