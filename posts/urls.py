6686011b8ffb6510d5f0ca2f59311ad6665b68a6
KangMinju <demo1208@gmail.com>
2023-08-23 11:52:56 +0900

    read(1)

--- posts/urls.py
+++ posts/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('',views.index, name='index'), # ''에 아무것도 안적으면 경로 posts/만 적어짐.
     # 이제 app_name, name을 변수로 사용해서 url 을 작성할 예정
     # ex. 항상 /posts/index 이런식으로 작성했던 것 대신 저 변수 사용 (변경할 때 돌아다니면서 하나하나 바꿀 필요 사라짐)
-    
+    path('<int:id>/', views.detail, name='detail')
 ]
