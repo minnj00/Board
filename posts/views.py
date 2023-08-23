47e1a6efd7ed2b847799bcd529ed592cbeb62f94
KangMinju <demo1208@gmail.com>
2023-08-23 14:26:08 +0900

    delete기능구현

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from .models import Post
 
 # Create your views here.
@@ -19,3 +19,24 @@ def detail(request, id):
     return render(request, 'detail.html', context)
 def new(request):
     return render(request, 'new.html')
+
+def create(request):
+    title=request.POST.get('title')
+    content=request.POST.get('content')
+
+    # post = Post()
+    # post.title = title
+    # post.content = content
+    # post.save 
+
+    # 인스턴스화를 함과 동시에 집어넣음
+    post= Post(title=title, content= content)
+    post.save()
+
+    return redirect('posts:detail', id=post.id)
+
+def delete(request, id):
+    post = Post.objects.get(id=id)
+    post.delete()
+
+    return redirect('posts:index')
